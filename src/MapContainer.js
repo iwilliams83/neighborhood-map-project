4e89f6fe4c32d9a26dca8a1d8fb5c5805a6e8707
Indira Williams <indirajamurath@gmail.com>
2018-10-25 13:05:45 -0400

    cleaned up some files

---
+++ src/MapContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class MapContainer extends Component {
+  render(){
+    return <div>
+    I'm a Map Container
+    </div>
+  }
+}
+
+export default MapContainer
